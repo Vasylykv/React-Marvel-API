8fb8f430ca3aca09a04331d8da1ec6a0f2b545a6
Vitaliy <fygetoid@gmail.com>
2022-04-16 04:54:17 +0300

    Implemented Tab indexing and Char selecting

--- src/components/charInfo/CharInfo.js
+++ src/components/charInfo/CharInfo.js
@@ -1,5 +1,6 @@
 import { Component } from 'react';
 import MarvelService from '../../services/MarvelService';
+import PropTypes from 'prop-types';
 
 import Spinner from '../spinner/Spinner';
 import ErrorMessage from '../errorMessage/ErrorMessage';
@@ -114,6 +115,10 @@ const View = ({ char }) => {
       </ul>
     </>
   );
+};
+
+CharInfo.propTypes = {
+  charId: PropTypes.number,
 };
 
 export default CharInfo;
--- src/components/charList/CharList.js
+++ src/components/charList/CharList.js
@@ -1,4 +1,6 @@
 import { Component } from 'react';
+import PropTypes from 'prop-types';
+
 import MarvelService from '../../services/MarvelService';
 import Spinner from '../spinner/Spinner';
 import ErrorMessage from '../errorMessage/ErrorMessage';
@@ -57,8 +59,22 @@ class CharList extends Component {
     });
   };
 
+  itemRefs = [];
+
+  setRef = (ref) => {
+    this.itemRefs.push(ref);
+  };
+
+  focusOnItem = (id) => {
+    this.itemRefs.forEach((item) =>
+      item.classList.remove('char__item_selected')
+    );
+    this.itemRefs[id].classList.add('char__item_selected');
+    this.itemRefs[id].focus();
+  };
+
   renderItems(arr) {
-    const items = arr.map((item) => {
+    const items = arr.map((item, i) => {
       let imgStyle = { objectFit: 'cover' };
       if (
         item.thumbnail ===
@@ -70,8 +86,20 @@ class CharList extends Component {
       return (
         <li
           className="char__item"
+          ref={this.setRef}
+          tabIndex={0}
           key={item.id}
-          onClick={() => this.props.onCharSelected(item.id)}
+          onClick={() => {
+            this.props.onCharSelected(item.id);
+            this.focusOnItem(i);
+          }}
+          onKeyDown={(e) => {
+            if (e.key === ' ' || e.key === 'Enter') {
+              e.preventDefault();
+              this.props.onCharSelected(item.id);
+              this.focusOnItem(i);
+            }
+          }}
         >
           <img src={item.thumbnail} alt={item.name} style={imgStyle} />
           <div className="char__name">{item.name}</div>
@@ -83,14 +111,8 @@ class CharList extends Component {
   }
 
   render() {
-    const {
-      loading,
-      error,
-      charList,
-      newItemLoading,
-      offset,
-      charEnded,
-    } = this.state;
+    const { loading, error, charList, newItemLoading, offset, charEnded } =
+      this.state;
 
     const items = this.renderItems(charList);
 
@@ -116,5 +138,9 @@ class CharList extends Component {
     );
   }
 }
+
+CharList.propTypes = {
+  onCharSelected: PropTypes.func.isRequired,
+};
 
 export default CharList;
